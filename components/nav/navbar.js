ac22da9eacb163eccef427fcc82f5c7745afa5e0
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2023-02-14 22:36:17 +0100

    discord

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="relative text-white flex justify-between items-center left-0 h-20 w-full p-8 bg-nav-color">
+    <nav className="relative left-0 flex items-center justify-between w-full h-20 p-8 text-white bg-nav-color">
       <img onClick={ () => { window.open("https://caballabs.com/", "_blank")}} className="w-16 h-16 cursor-pointer" src="logo192.png" alt="logo" />
-      <div className="flex space-between items-center">
-        <div onClick={ () => { window.open("https://discord.gg/Jv9Za474", "_blank")}} className=" hover:bg-b-color-hover active:bg-b-color-active flex p-4 relative items-start flex-shrink-0 border-solid border-2 cursor-pointer border-white rounded-lg w-fit ">
+      <div className="flex items-center space-between">
+        <div onClick={ () => { window.open("https://discord.gg/qJ3FBSQQ8T", "_blank")}} className="relative flex items-start flex-shrink-0 p-4 border-2 border-white border-solid rounded-lg cursor-pointer  hover:bg-b-color-hover active:bg-b-color-active w-fit">
           <span>
             <span>Join The Cabal</span>
           </span>
